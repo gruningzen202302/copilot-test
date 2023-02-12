83d8921b8984a335a258577444d65a188ef336d1
gruningzen202302 <gruningzen202302@gmail.com>
2023-02-11 23:54:41 -0300

    test:use copilot comments to test median function

--- clojure.clj
+++ clojure.clj
@@ -1,4 +1,5 @@
 ; This is a sandbox for clojure
+
 ;make a function that prints the most repeated vaue in a series of ten values
 (defn mode [a b c d e f g h i j]
   (let [count (fn [x]
@@ -13,3 +14,10 @@
 )
   
 (media-mode 5 5 5 5 5 5 5 1 1 1) 
+
+;make a function that prints the median of ten values
+(defn median [a b c d e f g h i j]
+  (let [sorted (sort a b c d e f g h i j)]
+    (println (/ (+ (nth sorted 4) (nth sorted 5)) 2))))
+;call the function
+(median 5 5 5 5 5 5 5 1 1 1)
--- sandbox.py
+++ sandbox.py
@@ -11,3 +11,47 @@ def linear_regression(x,y): #x and y are lists of data
     intercept = (y_sum - slope*x_sum)/n
     return slope, intercept
 
+#make a function that prints in separate rows the mean median and mode of ten values
+def mean_median_mode(): #no arguments
+    import statistics
+    import random
+    data = [random.randint(1,100) for i in range(10)]
+    print("Mean: ", statistics.mean(data))
+    print("Median: ", statistics.median(data))
+    print("Mode: ", statistics.mode(data))
+
+#call the functions and print the results   
+print(linear_regression([1,2,3,4,5],[1,2,3,4,5]))
+mean_median_mode()
+
+# Python program to print
+# median of elements
+ 
+# list of elements to calculate median
+n_num = [1, 2, 3, 4, 5]
+n = len(n_num)
+n_num.sort()
+ 
+if n % 2 == 0:
+    median1 = n_num[n//2]
+    median2 = n_num[n//2 - 1]
+    median = (median1 + median2)/2
+else:
+    median = n_num[n//2]
+print("Median is: " + str(median))
+
+#make a function that prints the median of ten values without using statistics
+def median(): #no arguments
+    import random   
+    data = [random.randint(1,100) for i in range(10)]   
+    data.sort()
+    if len(data) % 2 == 0:
+        median1 = data[len(data)//2]
+        median2 = data[len(data)//2 - 1]
+        median = (median1 + median2)/2
+    else:   
+        median = data[len(data)//2]
+    print("Median is: " + str(median))
+
+#call the function and print the results
+median()
